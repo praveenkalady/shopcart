7a9a26548c243ccc6a996c88102afe1d5a82d296
praveenkalady <praveenkvinfo@gmail.com>
2021-01-22 10:30:51 +0530

    initialized project

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const App = () => {
+    return (
+        <>
+            <Header/>
+            <h1>Hello Content</h1>
+            <Footer/>
+        </>
+    )
+}
+
+export default App
+
